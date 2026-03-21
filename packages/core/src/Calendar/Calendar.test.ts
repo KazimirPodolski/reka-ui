34253e784e2dfb19852cc438f5c1627e8c8128a0
Eduard Predescu <eduard.florin.predescu@gmail.com>
2026-03-21 16:13:54 +0200

    feat(date): migrate after rebase

--- packages/core/src/Calendar/Calendar.test.ts
+++ packages/core/src/Calendar/Calendar.test.ts
@@ -965,8 +965,8 @@ describe('calendar - tabindex states', () => {
     const { getByTestId } = setup({
       calendarProps: {
         modelValue: calendarDate,
-        minValue: new CalendarDate(1980, 1, 15),
-        maxValue: new CalendarDate(1980, 1, 19),
+        minValue: Temporal.PlainDate.from({ year: 1980, month: 1, day: 15 }),
+        maxValue: Temporal.PlainDate.from({ year: 1980, month: 1, day: 19 }),
       },
     })
 
@@ -978,7 +978,7 @@ describe('calendar - tabindex states', () => {
     const { getByTestId } = setup({
       calendarProps: {
         placeholder: calendarDate,
-        maxValue: new CalendarDate(1980, 1, 19),
+        maxValue: Temporal.PlainDate.from({ year: 1980, month: 1, day: 19 }),
       },
     })
 
